0edcac7572eb8427ca0e04d6f4ec85bd3254d8b9
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-12-23 14:44:07 -0300

    Agrega mejoras al componente Carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,47 +1,104 @@
 import styles from "./carousel.module.css";
 import projects from "../../utils/projects.json";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LanguageIcon from "@mui/icons-material/Language";
 
 const Carousel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.carousel}>
-        {projects.data.map((project, index) => (
-          <div className={styles.cardContainer} key={index}>
-            <div className={styles.card}>
-              <div className={styles.frontContent}>
-                <img
-                  src={project.image}
-                  alt="Si ves esto no cargó la imagen"
-                  className={styles.img}
-                />
-              </div>
-              <div className={styles.content}>
-                <p className={styles.heading}>{project.name}</p>
-                <p>{project.type}</p>
-                <div className={styles.iconContainer}>
-                  <a
-                    href={project.repo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <GitHubIcon className={styles.icon} />
-                  </a>
-                  {project.deploy && (
+        <Slider {...settings}>
+          {projects.data.map((project, index) => (
+            <div className={styles.cardContainer} key={index}>
+              <div className={styles.card}>
+                <div className={styles.frontContent}>
+                  <img
+                    src={project.image}
+                    alt="Si ves esto no cargó la imagen"
+                    className={styles.img}
+                  />
+                </div>
+                <div className={styles.content}>
+                  <p className={styles.heading}>{project.name}</p>
+                  <p>{project.type}</p>
+                  <div className={styles.iconContainer}>
                     <a
-                      href={project.deploy}
+                      href={project.repo}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <LanguageIcon className={styles.icon} />
+                      <GitHubIcon className={styles.icon} />
                     </a>
-                  )}
+                    {project.deploy && (
+                      <a
+                        href={project.deploy}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <LanguageIcon className={styles.icon} />
+                      </a>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
     </div>
   );
