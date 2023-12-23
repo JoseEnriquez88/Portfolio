f7a78ded25c019ccf75a5df22900f227131f92fe
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-12-23 16:24:57 -0300

    resposive carousel added

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -7,50 +7,22 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LanguageIcon from "@mui/icons-material/Language";
 
 const Carousel = () => {
-  var settings = {
+  const settings = {
     dots: true,
     infinite: true,
-    centerMode: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 4,
-    initialSlide: 0,
+    slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
       {
         breakpoint: 850,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 1,
         },
       },
       {
         breakpoint: 720,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
