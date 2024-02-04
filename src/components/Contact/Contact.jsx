e79ddee07d4f7edc9bdb5469959de2aad607c2ab
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-02-04 03:04:16 -0300

    update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,101 @@
 "use client";
 import styles from "./contact.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Footer from "../Footer/Footer";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const { NEXT_PUBLIC_SERVICE_ID, NEXT_PUBLIC_TEMPLATE_ID, NEXT_PUBLIC_USER_ID } =
+  process.env;
 
 const Contact = () => {
+  const formRef = useRef();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
+
+  const successNotify = () => {
+    toast.success("Thanks for communicating. I'll be in touch soon", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
+  const errorNotify = () => {
+    toast.error("There was an error. Please try again", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
+  const warnNotify = () => {
+    toast.warn("Make sure you have completed all fields.", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (
+      formData.name === "" ||
+      formData.email === "" ||
+      formData.message === ""
+    ) {
+      warnNotify();
+      return;
+    }
+
+    await emailjs
+      .sendForm(
+        "service_wd4xr5o",
+        "template_rliwto9",
+        formRef.current,
+        "vxpkT-TbZOwb3eaHL"
+      )
+      .then(
+        (result) => {
+          setFormData({
+            name: "",
+            email: "",
+            message: "",
+          });
+          successNotify();
+        },
+        (error) => {
+          errorNotify();
+        }
+      );
+  };
 
   return (
     <div className={styles.container}>
@@ -21,11 +108,12 @@ const Contact = () => {
             <img src="/SVG/contact.png" alt="image" className={styles.image} />
           </div>
           <div className={styles.formContainer}>
-            <form className={styles.form}>
+            <form ref={formRef} onSubmit={handleSubmit} className={styles.form}>
               <input
                 type="text"
                 name="name"
                 value={formData.name}
+                onChange={handleChange}
                 placeholder="Name"
                 className={styles.input}
               />
@@ -33,6 +121,7 @@ const Contact = () => {
                 type="email"
                 name="email"
                 value={formData.email}
+                onChange={handleChange}
                 placeholder="Email"
                 className={styles.input}
               />
@@ -40,10 +129,12 @@ const Contact = () => {
                 name="message"
                 rows={8}
                 value={formData.message}
+                onChange={handleChange}
                 placeholder="Message"
                 className={styles.textarea}
               />
               <button className={styles.button}>Send</button>
+              <ToastContainer />
             </form>
           </div>
         </div>
---
+++ src/utils/toastify.js
@@ -0,0 +1,45 @@
+import { toast } from "react-toastify";
+
+const successNotify = () => {
+  toast.success("Thanks for communicating. I'll be in touch soon", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
+
+const errorNotify = () => {
+  toast.error("There was an error. Please try again", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
+
+const warnNotify = () => {
+  toast.warn("Make sure you have completed all fields.", {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
+
+export default { successNotify, errorNotify, warnNotify };
