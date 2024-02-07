ec664611be01d955f60cc5b49885802a5179fe12
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-02-07 07:10:19 -0300

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,15 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "JE Portfolio",
-  description: "Jose Carlos Enriquez Dominguez Porfolio",
+  title: "Jose Enriquez - Portfolio",
+  description: "Jose Carlos Enriquez Dominguez Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link
-        rel="icon"
-        href="/logo.png"
-        type="image/png"
-        sizes="32x32"
-      />
+      <link rel="preconnect" href="/fonts/Prototype.ttf" />
+      <link rel="icon" href="/logo.png" type="image/png" sizes="32x32" />
       <body>{children}</body>
     </html>
   );
