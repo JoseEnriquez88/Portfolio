83c601ece27825a749f051a783aa8bb57477e209
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-02-07 07:15:29 -0300

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="preconnect" href="/fonts/Prototype.ttf" />
+      <head>
+        <link href="/fonts/Prototype.ttf" rel="stylesheet" />
+      </head>
       <link rel="icon" href="/logo.png" type="image/png" sizes="32x32" />
       <body>{children}</body>
     </html>
